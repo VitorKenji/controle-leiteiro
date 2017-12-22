5f957310dd8b8b5c560a475036c1b760a2b49b70
VitorKenji <kenji.vitor.tanno@gmail.com>
2017-12-22 14:52:25 -0200

    001 - criação metodo adicionar animal, busca por brinco de animal e adição de vacas (incompleto)

--- animal_controler.php
+++ animal_controler.php
@@ -1,11 +1,29 @@
 <?php
  	
-	function adicionar_vaca($conexao, $nome, $brinco, $origem, $data_nacimento, $peso_nacenca, $peso_desmama, $cor, $raca, $idade){
+	function adicionar_animal($conexao, $nome, $brinco, $origem, $data_nacimento, $peso_nacenca, $peso_desmama, $cor, $raca, $idade){
 
 		$query = "insert into animal (nome, brinco, origem, nacimento, peso_nacenca, peso_desmama, cor, raca, idade)
 		values ('{$nome}', {$brinco}, '{$origem}', '{$data_nacimento}', '{$peso_nacenca}', {$peso_desmama}, '{$cor}', '{$raca}', {$idade})";
 
 		return mysqli_query($conexao, $query);
+	}
+
+	function adicionar_vaca($id){
+
+		$query = "insert into vaca (id) values ('{$id}');";
+		return mysqli_query($conexao, $query);
+	}
+
+	function busca_animal($brinco){
+
+		$resultado = array();
+		$query = mysqli_query($conexao, "select id from animal where '{$brinco}' = brinco;");
+
+		while($resultado = mysqli_fetch_assoc($query)) {
+			array_push($produtos, $produto);
+		}
+		return $produtos;
+
 	}
 
 	function listar_vacas($conexao){
